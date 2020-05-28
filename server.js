8a54b475e643a782f15b2c5245bbaa601bce5dc0
lolpixel12 <66032488+lolpixel12@users.noreply.github.com>
2020-05-27 23:59:48 -0400

    Create server.js
    Help me please

---
+++ server.js
@@ -0,0 +1,16 @@
+/home/seboupixel/Escritorio/BOT/node_modules/discord.js/src/client/Client.js:41
+    } catch {
+            ^
+
+SyntaxError: Unexpected token {
+    at createScript (vm.js:80:10)
+    at Object.runInThisContext (vm.js:139:10)
+    at Module._compile (module.js:616:28)
+    at Object.Module._extensions..js (module.js:663:10)
+    at Module.load (module.js:565:32)
+    at tryModuleLoad (module.js:505:12)
+    at Function.Module._load (module.js:497:3)
+    at Module.require (module.js:596:17)
+    at require (internal/module.js:11:18)
+    at Object.<anonymous> (/home/seboupixel/Escritorio/BOT/node_modules/discord.js/src/index.js:8:11)
+    
